26822ef31312a04324fe7293c1db4e5c755b77cb
Théophile Gaudin <theophile.gaudin@gmail.com>
2017-01-07 14:34:58 +0100

    Adding a switch class to drive th SSR relay

---
+++ switch.rb
@@ -0,0 +1,34 @@
+require 'wiringpi'
+
+class Switch
+	def initialize(pin, gpio=nil)
+		if gpio.nil?
+			@gpio = WiringPi::GPIO.new
+		else
+			@gpio = gpio
+		end
+		@pin = pin
+		@gpio.pin_mode(pin, WiringPI::OUTPUT)
+		@gpio.digital_write(@pin, 0)
+		@status = 0		
+	end
+
+	def on
+		@gpio.digital_write(@pin, 1)
+		@status = 1
+	end
+
+	def off
+		@gpio.digital_write(@pin, 0)
+		@status = 0
+	end
+
+	def toggle
+		if (status == 1)
+			off
+		else
+			on
+		end
+	end
+end
+
